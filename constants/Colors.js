81b3cb503cadf665a07e489d1bf36e9443844544
Eduardo <ebmcangrejo97@gmail.com>
2021-02-20 20:54:50 -0300

    Adding flatlist grid and fixing the last row bug

---
+++ constants/Colors.js
@@ -0,0 +1,6 @@
+export default {
+    primaryColor: 'black',
+    accentColor: '#30343F',
+    thirdColor: '#89023E',
+    whiteColor: '#ffffff'
+};
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,15 +1,54 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import COLORS from '../constants/Colors';
+
+import { CATEGORIES } from "../data/dummy-data";
+import Category from '../models/category';
+
+const WIDTH = Dimensions.get('window').width;
+
+const formatData = (data, numColumns) => {
+    const numberOfFullRows = Math.floor(data.length / numColumns);
+
+    let numberOfElementsLastRow = data.length - (numberOfFullRows * numColumns);
+    while (numberOfElementsLastRow !== numColumns && numberOfElementsLastRow !== 0) {
+        data.push(new Category('null'));
+        numberOfElementsLastRow++;
+    }
+
+    return data;
+};
+let numOfColumns = 3;
 
 const CategoriesScreen = props => {
-    console.log(props);
+
+    const renderGridItem = (itemData) => {
+        if (itemData.item.id === 'null') {
+
+            return <View style={[styles.gridItem, { backgroundColor: itemData.item.color }]} />;
+
+        }
+        return (
+            <TouchableOpacity
+                onPress={() => {
+                    props.navigation.navigate({ routeName: 'CategoryMeals' });
+                }}
+                style={[styles.gridItem, { backgroundColor: itemData.item.color }]}>
+                <View >
+                    <Text style={{ color: COLORS.whiteColor }}>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
+
     return (
-        <View style={styles.screen}>
-            <Text>The CategoriesScreen</Text>
-            <Button title='Go to meals' onPress={() => { 
-                props.navigation.navigate({routeName: 'CategoryMeals'});
-            }} />
-        </View>
+        <FlatList
+            keyExtractor={(item, index) => item.id} //New react apps doesnt need this shit
+            data={formatData(CATEGORIES, numOfColumns)}
+            renderItem={renderGridItem}
+            numColumns={numOfColumns} />
     );
 };
 
@@ -18,8 +57,28 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 });
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: COLORS.primaryColor
+    },
+    headerTintColor: COLORS.whiteColor,
+    headerTitleStyle: {
+        textAlign: 'center',
+        fontFamily: 'open-sans',
+        fontWeight: 'bold',
+    },
+};
 
 export default CategoriesScreen;
 
