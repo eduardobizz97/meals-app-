451ccfea234a1bfdd047f001ab4963203d7b6066
Eduardo <ebmcangrejo97@gmail.com>
2021-03-02 14:51:16 -0300

    Working in the filterScreen

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Switch } from 'react-native-gesture-handler';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../components/HeaderButton';
+import Colors from '../constants/Colors';
+
+const FilterSwitch = props => {
+    return (
+        <View style={styles.filterContainer}>
+            <Text>{props.label}</Text>
+            <Switch
+                value={props.state}
+                onValueChange={props.onChange}
+                trackColor={{ false: Colors.thirdColor, true: Colors.thirdColor }}
+
+            />
+        </View>
+    );
+}
 
 const FiltersScreen = () => {
+    const [isGlutenFree, setIsGlutenFree] = useState(false);
+    const [isLactoseFree, setIsLactoseFree] = useState(false);
+    const [isVegan, setIsVegan] = useState(false);
+    const [isVegetarian, setIsVegetarian] = useState(false);
+
     return (
         <View style={styles.screen}>
-            <Text>The FiltersScreen</Text>
+            <Text style={styles.title}>Available Filters / Restrictions</Text>
+            <FilterSwitch 
+                label='Gluten-free' 
+                state={isGlutenFree} 
+                onChange={newValue => setIsGlutenFree(newValue)}
+            />
+            <FilterSwitch 
+                label='Lactose-free' 
+                state={isLactoseFree} 
+                onChange={newValue => setIsLactoseFree(newValue)}
+            />
+            <FilterSwitch 
+                label='Vegan' 
+                state={isVegan} 
+                onChange={newValue => setIsVegan(newValue)}
+            />
+            <FilterSwitch 
+                label='Vegetarian' 
+                state={isVegetarian} 
+                onChange={newValue => setIsVegetarian(newValue)}
+            />
+            
         </View>
     );
 };
@@ -21,23 +63,36 @@ FiltersScreen.navigationOptions = navData => {
                 <Item
                     title='Menu'
                     iconName='ios-menu'
-                    
+
                     onPress={() => {
                         navData.navigation.toggleDrawer();
                     }} />
-                    
+
             </HeaderButtons>
         )
 
     };
-    
+
 };
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
+
         alignItems: 'center'
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        margin: 20,
+        textAlign: 'center'
+    },
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '80%',
+        margin: 10
     }
 });
 
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import CustomHeaderButton from '../components/HeaderButton';
@@ -8,27 +9,32 @@ import CustomHeaderButton from '../components/HeaderButton';
 
 
 
-const MealDetailScreen = props => {
-    const { id, categoryIds, title, steps } = props.navigation.getParam('item');
-    return (
 
-        <View style={styles.screen}>
-            <View style={styles.detailContainer}>
-                <View style={styles.detailRow}>
-                    <Text>{title}</Text>
+
+const MealDetailScreen = props => {
+    const { id, categoryIds, imageUrl, title, steps } = props.navigation.getParam('item');
+    console.log(props.navigation.getParam('item'))
+    return (
+        <ScrollView>
+            <Image source={{uri: imageUrl}} style={styles.image}/>
+            <View style={styles.screen}>
+                <View style={styles.detailContainer}>
+                    <View style={styles.detailRow}>
+                        <Text>{title}</Text>
+                    </View>
+                    <View style={styles.detailRow}>
+                        {steps.map((item, index) => {
+                            return (
+                                <View key={index}>
+                                    <Text>{`${index + 1}.- ${item}`}</Text>
+                                </View>
+                            );
+                        })}
+                    </View>
+
                 </View>
-                <View style={styles.detailRow}>
-                    {steps.map((item, index) => {
-                        return (
-                            <View key={index}>
-                                <Text>{`${index + 1}.- ${item}`}</Text>
-                            </View>
-                        );
-                    })}
-                </View>
-                
             </View>
-        </View>
+        </ScrollView>
     );
 };
 
@@ -36,15 +42,15 @@ MealDetailScreen.navigationOptions = (navigationData) => {
     const item = navigationData.navigation.getParam('item');
     return {
         headerTitle: item.title,
-        headerRight: () => 
-        <HeaderButtons HeaderButtonComponent={ CustomHeaderButton }>
-            <Item 
-                title='favorite' 
-                iconName='ios-star' 
-                onPress={()=>{
-                    console.log('Mark as favorite')
-                }} />
-        </HeaderButtons>
+        headerRight: () =>
+            <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                <Item
+                    title='favorite'
+                    iconName='ios-star'
+                    onPress={() => {
+                        console.log('Mark as favorite')
+                    }} />
+            </HeaderButtons>
     };
 };
 
@@ -59,9 +65,13 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     detailRow: {
-       
+
+    },
+    image: {
+        width: '100%',
+        height: Dimensions.get('window').height / 3
     }
-    
+
 
 });
 
